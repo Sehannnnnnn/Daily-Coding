d1eeecc6d8456c6947f2f88752e155efa6b83f8e
Sehannnnnnn <stanley710@naver.com>
2022-10-19 12:19:12 +0900

    solve 20

--- 19_decryptCeaserCipher.js
+++ 19_decryptCeaserCipher.js
@@ -1,8 +1,25 @@
+// 문제
+// 암호화된 문자열과 암호화 키를 입력받아 복호화된 문자열을 리턴해야 합니다.
+// 카이사르 암호(Caesar cipher)는 평문(plaintext)을 암호키 secret개만큼 (오른쪽으로) 평행이동시켜 암호화 합니다. 복호화는 암호화된 문자열을 원래의 평문으로 복원하는 것을 말합니다.
+
+// 'hello'를 secret 3으로 암호화한 경우: 'khoor'
+// 입력
+// 인자 1 : str
+// string 타입의 알파벳 소문자 문자열
+// 인자 2 : secret
+// number 타입의 암호화 키
+// 출력
+// string 타입을 리턴해야 합니다.
+// 주의 사항
+// 빈 문자열을 입력받은 경우, 빈 문자열을 리턴해야 합니다.
+// 공백은 그대로 두어야 합니다.
+
 function decryptCaesarCipher(str, secret) {
     // TODO: 여기에 코드를 작성합니다.
     let [a,z] = ['a'.charCodeAt(0), 'z'.charCodeAt(0)]
     const strArr = str.split('');
     const decryptArr = strArr.map(char => {
+        if (char == '') return char;
         let unicode = char.charCodeAt(0)-secret;
         if (unicode < a) {
             return String.fromCharCode(z-(a-unicode-1));
---
+++ 20_compressString.js
@@ -0,0 +1,42 @@
+// 문제
+// 문자열을 입력받아 연속되는 문자가 있을 경우, 연속 구간을 반복되는 수와 문자로 조합한 형태로 압축한 문자열을 리턴해야 합니다.
+
+// 입력
+// 인자 1 : str
+// string 타입의 알파벳 문자열
+// 출력
+// string 타입을 리턴해야 합니다.
+// 주의 사항
+// 빈 문자열을 입력받은 경우, 빈 문자열을 리턴해야 합니다.
+// 3개 이상 연속되는 문자만 압축합니다.
+
+function compressString(str) {
+    // TODO: 여기에 코드를 작성합니다.
+    let output = ""
+    let count = 1;
+    let arr = str.split('');
+    arr.push(' ');
+    let prevStr = arr[0];
+  
+    for (let i = 1; i<arr.length+1; i++) {
+      if (prevStr == arr[i]) {
+        //앞과 현재가 같을때
+        count ++;
+      } else {
+        //앞과 현재가 다를때
+        if (count > 2) output += count + prevStr;
+        else if (count == 2) output += prevStr.repeat(2);
+        else output += prevStr;
+        count = 1;
+      }
+      prevStr = arr[i]
+    }
+    output.trim();
+    return output;
+  }
+  
+  let output = compressString('wwwggopp');
+  console.log(output); // --> abc
+  
+  output = compressString('wwwggoppopppp');
+  console.log(output); // --> 3wggoppo4p  
