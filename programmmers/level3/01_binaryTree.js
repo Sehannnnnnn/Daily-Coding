1b56432b48b1eb68e482dbad8aff2c0a1bc4dcb6
Sehan, Kim <stanley710@naver.com>
2023-02-26 13:28:07 +0900

    lv 3 solve

--- programmmers/level3/01_binaryTree.js
+++ programmmers/level3/01_binaryTree.js
@@ -9,19 +9,38 @@ function solution(numbers) {
         // 이진수 앞에 0 추가
         let bi_tree = '0'.repeat(2**m-1 - n);
         bi_tree = bi_tree + '' + bi_num;
-
-        checkBTree(bi_tree, 0, bi_tree.length-1)
-        answer.push(checkBTree(bi_tree));
+        console.log(bi_tree, number)
+        if (checkBTree(bi_tree, 0, bi_tree.length-1)) {
+            answer.push(1);
+        }
+        else {
+            answer.push(0);
+        }
     }
     console.log(answer);
     return answer;
 }
 
-function checkBTree (b_tree, left, right) {
-    if (left == right) return console.log(left);
-    let mid = Math.floor(left+right/2);
-    checkBTree(b_tree, left, mid);
-    checkBTree(b_tree, mid, right);
+function checkBTree (b_str, start, end) {
+    const mid = Math.floor((start+end) / 2);
+    //자식 노드 idx
+    const left_c = Math.floor((start + mid-1)/2);
+    const right_c = Math.floor((mid+1 + end)/2);
+    
+    if (start == end) {
+        console.log(mid);
+        return true;
+    }
+    console.log(left_c, mid, right_c);
+    if (b_str[mid] === '0' && ((b_str[left_c] === '1') || (b_str[right_c] === '1'))) {
+        return false;
+    }
+
+    if (!checkBTree(b_str, start, mid-1)) return false;
+    if (!checkBTree(b_str, mid+1, end)) return false;
+    return true;
 }
 
+
 solution([63, 111, 95])
+
